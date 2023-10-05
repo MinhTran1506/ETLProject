e6a19e22a89dcacbf436f0c8680b029bb4836a94
MinhTran1506 <Your GitHub Email>
2023-10-05 13:06:01 +0700

    data pipeline

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ DB_DETAILS = {
     'dev' : {
         'SOURCE_DB' : {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '139.99.289.131',
+            'DB_HOST': '172.16.6.33',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
             'DB_PASS': os.environ.get('SOURCE_DB_PASS')
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '139.99.289.131',
+            'DB_HOST': '172.16.6.33',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
             'DB_PASS': os.environ.get('TARGET_DB_PASS')
